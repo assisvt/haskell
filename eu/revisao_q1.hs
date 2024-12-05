238ba011626d3d6cb737938f252294e0af6497e2
Vitor Assis <contatoassisvt@gmail.com>
2024-12-04 23:31:12 -0300

    Update revisao_q1.hs

--- eu/revisao_q1.hs
+++ eu/revisao_q1.hs
@@ -4,11 +4,7 @@ totalPlantas arvoresIniciais taxaCrescimento 0 = arvoresIniciais  -- Caso base: 
 totalPlantas arvoresIniciais taxaCrescimento anos =
   totalPlantas (arvoresIniciais * (1 + taxaCrescimento)) taxaCrescimento (anos - 1)
 {-
-Copiar código
-let consumos = [50, 120, 80, 150, 200]
--- Resultado esperado:
--- Custo por residência: [125.0, 300.0, 200.0, 375.0, 500.0]
--- Consumo acima de 100: [120, 150, 200]
--- Custo total: 1500.0
+totalPlantas 1000 0.05 3
+-- Resultado: 1157
 -}
 
