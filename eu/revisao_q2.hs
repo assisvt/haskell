208b3f1fe221170737b27854a273eb492fc5957e
Vitor Assis <contatoassisvt@gmail.com>
2024-12-04 23:47:37 -0300

    Update revisao_q2.hs

--- eu/revisao_q2.hs
+++ eu/revisao_q2.hs
@@ -12,35 +12,38 @@ calcularCustos consumos = map (\consumo -> fromIntegral consumo * custoPorUnidad
 filtrarAltosConsumos :: [Int] -> [Int]
 filtrarAltosConsumos consumos = filter (> 100) consumos
 
--- Calcular o custo total (usando reduce -> foldl')
+-- Calcular o custo total de uma lista de consumos
 calcularCustoTotal :: [Int] -> Double
-calcularCustoTotal consumos =
-  foldl' (+) 0 (calcularCustos consumos)
+calcularCustoTotal consumos = foldl' (+) 0 (calcularCustos consumos)
 
 -- Função principal
 main :: IO ()
 main = do
-  let consumos = [50, 120, 80, 150, 200] -- Consumos de água das residências
-  
-  -- Cálculo do custo de cada residência
-  let custos = calcularCustos consumos
-  
-  -- Filtro de consumos acima de 100 unidades
-  let altosConsumos = filtrarAltosConsumos consumos
-  
-  -- Cálculo do custo total para consumos acima de 100 unidades
-  let custoTotal = calcularCustoTotal altosConsumos
+    let consumos = [50, 120, 80, 150, 200] -- Consumos de água das residências
+    
+    -- Cálculo do custo de cada residência
+    let custos = calcularCustos consumos
+    
+    -- Filtro de consumos acima de 100 unidades
+    let altosConsumos = filtrarAltosConsumos consumos
+    
+    -- Cálculo do custo total para todos os consumos
+    let custoTotalTodos = calcularCustoTotal consumos
+    
+    -- Cálculo do custo total para consumos acima de 100 unidades
+    let custoTotalAltos = calcularCustoTotal altosConsumos
 
-  -- Exibir resultados
-  putStrLn $ "Custo por residência: " ++ show custos
-  putStrLn $ "Consumo acima de 100: " ++ show altosConsumos
-  putStrLn $ "Custo total: " ++ show custoTotal
-
+    -- Exibir resultados
+    putStrLn $ "Custo por residência: " ++ show custos
+    putStrLn $ "Consumo acima de 100: " ++ show altosConsumos
+    putStrLn $ "Custo total (todas as residências): " ++ show custoTotalTodos
+    putStrLn $ "Custo total (apenas residências acima de 100 unidades): " ++ show custoTotalAltos
   {-
-  Copiar código
-let consumos = [50, 120, 80, 150, 200]
+  let consumos = [50, 120, 80, 150, 200]
 -- Resultado esperado:
 -- Custo por residência: [125.0, 300.0, 200.0, 375.0, 500.0]
 -- Consumo acima de 100: [120, 150, 200]
--- Custo total: 1500.0 //erro, pq dá 1175
+-- Custo total (todas as residências): 1500.0
+-- Custo total (apenas residências acima de 100 unidades): 1175.0
+
   -}
